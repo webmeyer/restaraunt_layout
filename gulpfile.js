c139e2305393a7a1bff862c3bd4c97a98c2c2edf
R3re <KaJ2511001>
2019-08-31 15:00:55 +0500

    Добавил блоки: Signatures, Menu, Food slider

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,19 @@
 var gulp = require ('gulp'),
     sass = require ('gulp-sass'),
-    browserSync = require ('browser-sync');
-const autoprefixer = require ('gulp-autoprefixer');
+    browserSync = require ('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('scss', function (){
     return gulp.src('./sass/*.sass')
         .pipe(sass({outputStyle: 'expanded'}))
         .pipe(gulp.dest('./css'))
         .pipe(browserSync.reload({stream: true}))
-});
-
-gulp.task('autoprefix', function (){
-    gulp.src('css/*.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('./css'))
 });
+
 
 gulp.task('browser-sync', function (){
     browserSync.init({
